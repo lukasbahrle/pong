56baa2d9df9740c5e4017934ea69ae0c0e4d8467
Lukas Bahrle <lukasbahrle@gmail.com>
2022-12-09 21:30:06 +0100

    Initial Commit

---
+++ Pong/PongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PongApp.swift
+//  Pong
+//
+//  Created by Lukas Bahrle Santana on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
