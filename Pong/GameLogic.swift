c1d356bb8ccdda5fbc16410f4f07aac631792d47
Lukas Bahrle <lukasbahrle@gmail.com>
2022-12-10 16:05:01 +0100

    Add game state to game logic

--- Pong/GameLogic.swift
+++ Pong/GameLogic.swift
@@ -9,11 +9,20 @@ import Foundation
 import Combine
 
 class GameLogic {
+    private(set) var gameState: GameState = .readyToPlay {
+        didSet {
+            onScoreUpdate(score, score.isGameOver(target: targetScore))
+        }
+    }
+    let onGameStateUpdate: (_ state: GameState) -> Void
+    
     private(set) var score: GameScore {
         didSet {
             onScoreUpdate(score, score.isGameOver(target: targetScore))
         }
     }
+    let targetScore: Int
+    let onScoreUpdate: (_ score: GameScore, _ isGameOver: Bool) -> Void
     
     // MARK: - Game objects
     
@@ -21,13 +30,11 @@ class GameLogic {
     let player: GameObject = .paddle(true)
     let opponent: GameObject = .paddle(false)
     
-    let targetScore: Int
-    let onScoreUpdate: (_ score: GameScore, _ isGameOver: Bool) -> Void
-    
-    internal init(score: GameScore = GameScore.initialScore, targetScore: Int, onScoreUpdate: @escaping (_ score: GameScore, _ isGameOver: Bool) -> Void) {
+    internal init(score: GameScore = GameScore.initialScore, targetScore: Int, onScoreUpdate: @escaping (_ score: GameScore, _ isGameOver: Bool) -> Void, onGameStateUpdate: @escaping (_ gameState: GameState) -> Void) {
         self.score = score
         self.targetScore = targetScore
         self.onScoreUpdate = onScoreUpdate
+        self.onGameStateUpdate = onGameStateUpdate
     }
     
     func start() {}
---
+++ Pong/GameState.swift
@@ -0,0 +1,14 @@
+//
+//  GameState.swift
+//  Pong
+//
+//  Created by Lukas Bahrle Santana on 10/12/22.
+//
+
+import Foundation
+
+enum GameState {
+    case readyToPlay
+    case playing
+    case gameOver
+}
