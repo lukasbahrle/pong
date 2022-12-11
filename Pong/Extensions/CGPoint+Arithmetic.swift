a3db18c78131cf4b1334bf7f644302f69598b76a
Lukas Bahrle <lukasbahrle@gmail.com>
2022-12-11 21:04:26 +0100

    Add basic CGPoint operations extension

---
+++ Pong/Extensions/CGPoint+Arithmetic.swift
@@ -0,0 +1,31 @@
+//
+//  CGPoint+Arithmetic.swift
+//  Pong
+//
+//  Created by Lukas Bahrle Santana on 11/12/22.
+//
+
+import Foundation
+
+extension CGPoint {
+    static func - (left: CGPoint, right: CGPoint) -> CGPoint {
+      return CGPoint(x: left.x - right.x, y: left.y - right.y)
+    }
+    
+    static func -= (left: inout CGPoint, right: CGPoint) {
+      left = left - right
+    }
+    
+    static func / (left: CGPoint, value: TimeInterval) -> CGPoint {
+      return CGPoint(x: left.x / value, y: left.y / value)
+    }
+    
+    
+    static func * (left: CGPoint, value: CGFloat) -> CGPoint {
+      return CGPoint(x: left.x * value, y: left.y * value)
+    }
+    
+    static func *= (left: inout CGPoint, value: CGFloat) {
+      left = left * value
+    }
+}
