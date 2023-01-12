6bf9ef5eb8d25a3ac9c410b6f7a8ae2f8090786c
Lukas Bahrle Santana <lukasbahrle@gmail.com>
2023-01-12 23:15:36 +0100

    Add container view

--- Pong/View/JoinToPlayView.swift
+++ Pong/View/JoinToPlayView.swift
@@ -15,43 +15,46 @@ struct JoinToPlayView: View {
     @State private var isDisplayingSharingController: Bool = false
     
     var body: some View {
-        if gameState == .ready {
-            Button {
-                action()
-            } label: {
-                Text("Play")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 10)
-                    .background(Capsule())
-            }
-        }
-        else if case let .notReady(playerType) = gameState {
-            
-            if playerType == .opponent {
-                Label("Waiting for your opponnt to join", systemImage: "person.badge.clock.fill")
-                    .font(.headline)
-                    .foregroundColor(Color(white: 0.9))
-            }
-            else {
+        VStack {
+            if gameState == .ready {
                 Button {
-                    if groupStateObserver.isEligibleForGroupSession {
-                        action()
-                    }
-                    else {
-                        isDisplayingSharingController = true
-                    }
+                    action()
                 } label: {
-                    Label("Join to play", systemImage: "person.2.fill")
+                    Text("Play")
                         .font(.headline)
                         .foregroundColor(.white)
                         .padding(.horizontal, 20)
                         .padding(.vertical, 10)
                         .background(Capsule())
                 }
-                .sheet(isPresented: $isDisplayingSharingController) {
-                    ActivitySharingViewController()
+            }
+            else if case let .notReady(playerType) = gameState {
+                
+                if playerType == .opponent {
+                    Label("Waiting for your opponent to join", systemImage: "person.badge.clock.fill")
+                        .font(.headline)
+                        .foregroundColor(Color(white: 0.9))
+                        .background(Color.black)
+                }
+                else {
+                    Button {
+                        if groupStateObserver.isEligibleForGroupSession {
+                            action()
+                        }
+                        else {
+                            isDisplayingSharingController = true
+                        }
+                    } label: {
+                        Label("Join to play", systemImage: "person.2.fill")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                            .background(Capsule())
+                    }
+                    .sheet(isPresented: $isDisplayingSharingController) {
+                        ActivitySharingViewController()
+                    }
                 }
             }
         }
